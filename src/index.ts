eb85ac74deedf1fe074d9d9ab4a2c4124d6765ba
Lucas Otoni <lucas.otoni2004@gmail.com>
2023-09-04 11:49:54 -0300

    Sistema de aluguel de bike atualizado

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { App } from "./app";
+import { Bike } from "./bike";
+import { Rent } from "./rent";
+import { User } from "./user";
+
+const bike = new Bike('mountain bike', 'mountain', 123, 500, 100.5, 'desc', 5, [], '1');
+const user = new User('Maria', 'maria@mail.com', '1234');
+const today = new Date();
+const twoDaysFromToday = new Date();
+twoDaysFromToday.setDate(twoDaysFromToday.getDate() + 2);
+const tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
+const sevenDaysFromToday = new Date();
+sevenDaysFromToday.setDate(sevenDaysFromToday.getDate() + 7);
+const rent1 = Rent.create([], bike, user, today, twoDaysFromToday);
+const user2 = new User('Marta', 'marta@mail.com', '3123');
+const bike2 = new Bike('caloi', 'caloi zero', 23, 300, 98.9, 'desc', 5, [], "2");
+
+const app = new App();
+
+
+app.addUser(user2);
+const foundUser = app.findUser('marta@mail.com');
+console.log(foundUser)
+app.registraBike(bike2);
+const bikeFind = app.returnBike('2')
+console.log(bikeFind)
+app.addUser(user)
+app.registraBike(bike);
+const alugaBike = app.rentBike(bike, user2, today, twoDaysFromToday);
+console.log(alugaBike)
+app.removeUser(user2)
